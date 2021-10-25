35fcd6fc71ea0cbe26f96dd70315123133f5b5a2
Weilian Xu <William.Xu@covance.com>
2021-10-25 19:37:58 -0400

    Updated auth.js to check account status.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,23 @@
 // Functions
 
 function checkUserAuth(req, res, next) {
+
+    let feedback = {
+        title: "Access Denied!",
+        message: "",
+        buttonLink: "/",
+        buttonText: "Back"
+      }
+
     if (!req.isAuthenticated()) {
         res.status(401);
         return res.redirect("/sign-in");
+    }
+
+    if (req.user.status !== "active") {
+        res.status(403);
+        feedback.message = "You account is disabled. Please contact your administrator for help.";
+        return res.render("feedback", {feedback: feedback});
     }
 
     next();
@@ -14,7 +28,7 @@ function checkAdminAuth(req, res, next) {
     
     let feedback = {
         title: "Access Denied!",
-        message: "The Admin Portal is only open to users with the admin role. Please contact your administrator for help.",
+        message: "",
         buttonLink: "/",
         buttonText: "Back"
       }
@@ -24,8 +38,15 @@ function checkAdminAuth(req, res, next) {
         return res.redirect("/admin-sign-in");
     }
 
+    if (req.user.status !== "active") {
+        res.status(403);
+        feedback.message = "You account is disabled. Please contact your administrator for help.";
+        return res.render("feedback", {feedback: feedback});
+    }
+    
     if (req.user.role !== "admin") {
         res.status(403);
+        feedback.message = "The Admin Portal is only open to users with the admin role. Please contact your administrator for help.";
         return res.render("feedback", {feedback: feedback});
     }
 
